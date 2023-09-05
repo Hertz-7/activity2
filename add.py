85e6650e4e9b4020d362f537279de442998c4986
muhammadzainimran <muhammadzainimran1@gmail.com>
2023-09-05 11:24:30 +0500

    HO GAYA ADD

---
+++ add.py
@@ -0,0 +1,9 @@
+def add_numbers(a, b):
+    result = a + b
+    return result
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+add_result = add_numbers(num1, num2)
+print("The difference  is:", add_result)
